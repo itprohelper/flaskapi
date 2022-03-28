83ec4469283c791fc4d6d7c3a00583adeca89f9f
itprohelper <rudy@itprohelper.com>
2022-03-28 17:38:30 -0400

    added ability to store data in DB

--- web/sentences.py
+++ web/sentences.py
@@ -14,7 +14,7 @@ api = Api(app)
 
 client = MongoClient("mongodb://db:27017")
 db = client.SentencesDatabase
-UserNum = db["Users"]
+users = db["Users"]
 
 class Register(Resource):
     def post(self):
@@ -24,3 +24,65 @@ class Register(Resource):
         #Get the data
         username = postedData["username"]
         password = postedData["password"]
+
+        hashed_pw = hashed = bcrypt.hashpw(password, bcrypt.gensalt())
+
+        #Store username and password into the database
+        users.insert({
+            "Username": username,
+            "Password": hashed_pw,
+            "Sentence": "",
+            "Tokens":6
+        })
+
+        retJson = {
+            "status": 200,
+            "msg": "You successfully signed up for the API."
+        }
+        return jsonify(retJson)
+
+class Store(Resource):
+    def post(self):
+        #Step 1 get the posted data
+        postedData = request.get_json()
+
+        #Step 2 is to read the data
+        username = postedData["username"]
+        password = postedData["password"]
+        sentence = postedData["sentence"]
+
+        #Step 3 verify the username pw match
+        correct_pw = verifyPw(username, password)
+
+        if not correct_pw:
+            retJson = {
+                "status": 302
+            }
+            return jsonify(retJson)
+        #Step 4 verify user has enough Tokens
+        num_tokens = countTokens(username)
+        if num_tokens <= 0:
+            retJson = {
+                "status": 301
+            }
+            return jsonify(retJson)
+        #Step 5 store the sentence and return 200 OK
+        users.update({
+            "Username":username
+            }, {
+                "$set":{
+                "Sentence":sentence,
+                "Tokens":num_tokens-1
+                }
+            })
+
+            retJson = {
+                "status":200,
+                "msg":"Sentence saved successfully"
+            }
+            return jsonify(retJson)
+
+api.add_resource(Register, '/register')
+
+if __name__=="__main__":
+    app.run(host='0.0.0.0')
