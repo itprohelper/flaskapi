68de5dad37882733ae7179c786eee8fbd6a3a832
itprohelper <rudy@itprohelper.com>
2022-03-29 23:04:08 -0400

    get resource to get users sentences. still not working

--- web/sentences.py
+++ web/sentences.py
@@ -83,7 +83,7 @@ class Store(Resource):
             }
             return jsonify(retJson)
         #Step 5 store the sentence and return 200 OK
-        users.update({
+        users.update_one({
             "Username":username
         }, {
             "$set":{
@@ -98,8 +98,41 @@ class Store(Resource):
         }
         return jsonify(retJson)
 
+class Get(Resource):
+    def post(self):
+        postedData = request.get_json()
+
+        username = postedData["username"]
+        password = postedData["password"]
+
+        correct_pw = verifyPw(username, password)
+
+        if not correct_pw:
+            retJson = {
+                "status":302
+            }
+            return jsonify(retJson)
+
+        num_tokens = countTokens(username)
+        if num_tokens <= 0:
+            retJson = {
+                "status": 301
+            }
+            return jsonify(retJson)
+
+        sentence = users.find({
+            "username": username
+        })[0]["Sentence"]
+        retJson = {
+            "status":200,
+            "sentence": sentence
+        }
+
+        return jsonify(retJson)
+
 api.add_resource(Register, '/register')
 api.add_resource(Store,  '/store')
+api.add_resource(Get, '/get')
 
 if __name__=="__main__":
     app.run(host='0.0.0.0')
